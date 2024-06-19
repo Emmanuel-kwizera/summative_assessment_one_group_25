c38b181d759097002a1b47d0da26c8082a88e916
izabayo7 <cedricizabayo7@gmail.com>
2024-06-19 10:18:55 +0200

    feat: field validation done

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ if (age < 18 || age > 100) {
 if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
     errorMessage += '<p>Email format is invalid.</p>';
 }
+
+if (/[^a-zA-Z0-9 .,!?\n]/.test(feedback)) {
+    errorMessage += '<p>Feedback contains invalid characters.</p>';
+}
+if (rating < 1 || rating > 5) {
+    errorMessage += '<p>Rating must be between 1 and 5.</p>';
+}
+if (!date) {
+    errorMessage += '<p>Please select a valid date.</p>';
+}
